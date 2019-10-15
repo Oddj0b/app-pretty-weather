7107e7a850f7d3ba5017736a51a1120d358ef58e
Villars Gimm <villars.gimm@gmail.com>
2019-10-15 12:13:19 +0200

    Update post-build script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -5,8 +5,8 @@ find $APPCENTER_SOURCE_DIRECTORY -regex '*.UITest.*\.csproj' -exec echo {} \;
 echo
 echo "Building UI test projects:"
 find $APPCENTER_SOURCE_DIRECTORY -regex '*.UITEst.*\.csproj' -exec msbuild {} \;
-echo "Compiled projects to run UI tests:"
-find $APPCENTER_SOURCE_DIRECTORY -regex '*.bin.*UITest.*\.dll' -exec echo {} \;
+#echo "Compiled projects to run UI tests:"
+#find $APPCENTER_SOURCE_DIRECTORY -regex '*.bin.*UITest.*\.dll' -exec echo {} \;
 echo "Running UI test in App Center Test:"
 appPath= find APPCENTER_OUTPUT_DIRECTORY -regex '*.bin.*ipa' echo {}\;
 buildDir= find $APPCENTER_SOURCE_DIRECTORY -regex '*.bin.*UITEST.*\' -exec echo{}
