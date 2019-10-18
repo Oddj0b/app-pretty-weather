0702edd39f407819734566b9f6a4916c3742e15d
Villars Gimm <villars.gimm@gmail.com>
2019-10-18 09:35:27 +0200

    Move trailing backslash back a space

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -8,6 +8,6 @@ find $APPCENTER_SOURCE_DIRECTORY -regex '*.UITest.*\.csproj' -exec msbuild {} \;
 echo "Compiled projects to run UI tests:"
 find $APPCENTER_SOURCE_DIRECTORY -regex '*.bin.*UITest.*\.dll' -exec echo {} \;
 echo "Running UI test in App Center Test:"
-appPath= find APPCENTER_OUTPUT_DIRECTORY -regex '*.bin.*ipa' echo {} \;
+appPath= find $APPCENTER_OUTPUT_DIRECTORY -regex '*.bin.*ipa' echo {} \;
 buildDir= find $APPCENTER_SOURCE_DIRECTORY -regex '*.bin.*UITest.*\' -exec echo{}
 appcenter test run uitest --app $APP_OWNER --devices $DEVICE_SET --test-series "$APPCENTER_BRANCH-$APPCENTER_TRIGGER" --locale $LOCALE --app-path $appPath --build-dir $buildDir
