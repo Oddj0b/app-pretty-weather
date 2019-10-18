6715f6296bac03604820232400cb319d954880fe
Villars Gimm <villars.gimm@gmail.com>
2019-10-18 14:37:08 +0200

    Correct uitest-tools-dir name

--- PrettyWeather/PrettyWeather.iOS/appcenter-post-build.sh
+++ PrettyWeather/PrettyWeather.iOS/appcenter-post-build.sh
@@ -14,4 +14,4 @@ echo "What is in the source directory"
 ls $APPCENTER_SOURCE_DIRECTORY
 APPPATH=$APPCENTER_OUTPUT_DIRECTORY/PrettyWeather.iOS.ipa #-regex '*.bin.*ipa' echo {} \;
 BUILDDIR=$APPCENTER_SOURCE_DIRECTORY/PrettyWeather.UITest/bin/Debug/ #-regex '*.bin.*UITest.*\' -exec echo {} \;
-appcenter test run uitest --app $APP_OWNER --devices $DEVICE_SET --test-series "$APPCENTER_BRANCH-$APPCENTER_TRIGGER" --locale $LOCALE --app-path $APPPATH --build-dir $BUILDDIR --async --uitest-tool-dir $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools --token $APPCENTER_TOKEN
+appcenter test run uitest --app $APP_OWNER --devices $DEVICE_SET --test-series "$APPCENTER_BRANCH-$APPCENTER_TRIGGER" --locale $LOCALE --app-path $APPPATH --build-dir $BUILDDIR --async --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools --token $APPCENTER_TOKEN
