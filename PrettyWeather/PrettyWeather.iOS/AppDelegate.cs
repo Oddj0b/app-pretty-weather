fdc165a4b86cb36b755420d12d62eecf04181200
Villars Gimm <villars.gimm@gmail.com>
2019-10-18 15:08:24 +0200

    Formatting

--- PrettyWeather/PrettyWeather.iOS/AppDelegate.cs
+++ PrettyWeather/PrettyWeather.iOS/AppDelegate.cs
@@ -22,9 +22,9 @@ namespace PrettyWeather.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            #if ENABLE_TEST_CLOUD
-                Xamarin.Calabash.start()
-            #endif
+#if ENABLE_TEST_CLOUD
+Xamarin.Calabash.Start();
+#endif
             global::Xamarin.Forms.Forms.SetFlags("CollectionView_Experimental");
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
