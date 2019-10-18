9cde11e97cdffd28850f6906fb0827329fac4655
Villars Gimm <villars.gimm@gmail.com>
2019-10-18 14:44:52 +0200

    Remove ls command

--- PrettyWeather/PrettyWeather.iOS/appcenter-post-build.sh
+++ PrettyWeather/PrettyWeather.iOS/appcenter-post-build.sh
@@ -11,7 +11,6 @@ find $APPCENTER_SOURCE_DIRECTORY -regex '*.bin.*UITest.*\.dll' -exec echo {} \;
 echo "What is in the output directory"
 ls $APPCENTER_OUTPUT_DIRECTORY
 echo "What is in the source directory"
-ls $APPCENTER_SOURCE_DIRECTORY
 APPPATH=$APPCENTER_OUTPUT_DIRECTORY/PrettyWeather.iOS.ipa #-regex '*.bin.*ipa' echo {} \;
 BUILDDIR=$APPCENTER_SOURCE_DIRECTORY/PrettyWeather.UITest/bin/Debug/ #-regex '*.bin.*UITest.*\' -exec echo {} \;
 appcenter test run uitest --app $APP_OWNER --devices $DEVICE_SET --test-series "$APPCENTER_BRANCH-$APPCENTER_TRIGGER" --locale $LOCALE --app-path $APPPATH --build-dir $BUILDDIR --async --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools --token $APPCENTER_TOKEN
