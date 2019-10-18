2de1974b714b19407e6ac163cdbf0a1afe3f33d6
Villars Gimm <villars.gimm@gmail.com>
2019-10-18 12:19:15 +0200

    Move token to test run command

--- PrettyWeather/PrettyWeather.iOS/appcenter-post-build.sh
+++ PrettyWeather/PrettyWeather.iOS/appcenter-post-build.sh
@@ -7,8 +7,6 @@ echo "Building UI test projects:"
 find $APPCENTER_SOURCE_DIRECTORY -regex '*.UITest.*\.csproj' -exec msbuild {} \;
 echo "Compiled projects to run UI tests:"
 find $APPCENTER_SOURCE_DIRECTORY -regex '*.bin.*UITest.*\.dll' -exec echo {} \;
-echo "Logging into App Center"
-appcenter login --token $APPCENTER_TOKEN
 #echo "Running UI test in App Center Test:"
 echo "What is in the output directory"
 ls $APPCENTER_OUTPUT_DIRECTORY
@@ -16,4 +14,4 @@ echo "What is in the source directory"
 ls $APPCENTER_SOURCE_DIRECTORY
 APPPATH=find $APPCENTER_OUTPUT_DIRECTORY/PrettyWeather.iOS.ipa #-regex '*.bin.*ipa' echo {} \;
 BUILDDIR=find $APPCENTER_SOURCE_DIRECTORY -regex '*.bin.*UITest.*\' -exec echo {} \;
-#appcenter test run uitest --app $APP_OWNER --devices $DEVICE_SET --test-series "$APPCENTER_BRANCH-$APPCENTER_TRIGGER" --locale $LOCALE --app-path $APPPATH --build-dir $BUILDDIR --async
+#appcenter test run uitest --app $APP_OWNER --devices $DEVICE_SET --test-series "$APPCENTER_BRANCH-$APPCENTER_TRIGGER" --locale $LOCALE --app-path $APPPATH --build-dir $BUILDDIR --async --token $APPCENTER_TOKEN
