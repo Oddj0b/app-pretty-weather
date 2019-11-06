178333c954d9c1aba77304de4698113f4894dd84
Villars Gimm <villars.gimm@gmail.com>
2019-11-06 10:37:19 +0100

    Simple search for apk file

--- PrettyWeather/PrettyWeather.Android/appcenter-post-build.sh
+++ PrettyWeather/PrettyWeather.Android/appcenter-post-build.sh
@@ -10,7 +10,10 @@ find $APPCENTER_SOURCE_DIRECTORY -regex '*.bin.*UITest.*\.dll' -exec echo {} \;
 #echo "Running UI test in App Center Test:"
 echo "What is in the output directory"
 ls $APPCENTER_OUTPUT_DIRECTORY
+echo "Updating package name to $PACKAGE_NAME in AndroidManifest.xml"
+ANDROID_MANIFEST_FILE=$APPCENTER_SOURCE_DIRECTORY/Droid/Properties/AndroidManifest.xml
+sed -i '' 's/package="[^"]*"/package="'$PACKAGE_NAME'"/' $ANDROID_MANIFEST_FILE
 echo "What is in the source directory"
-APPPATH=$APPCENTER_OUTPUT_DIRECTORY/$APPCENTER_XAMARIN_PROJECT.apk #-regex '*.bin.*apk' echo {} \;
+APPPATH=$APPCENTER_OUTPUT_DIRECTORY/*.apk
 BUILDDIR=$APPCENTER_SOURCE_DIRECTORY/PrettyWeather.UITest/bin/Debug/ #-regex '*.bin.*UITest.*\' -exec echo {} \;
 appcenter test run uitest --app $APP_OWNER --devices $DEVICE_SET --test-series "$APPCENTER_BRANCH-$APPCENTER_TRIGGER" --locale $LOCALE --app-path $APPPATH --build-dir $BUILDDIR --async --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools --token $APPCENTER_TOKEN
